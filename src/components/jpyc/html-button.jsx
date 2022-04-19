2f2c3dc75648d8fd7c19e2eddf719cf98544df25
i <i@is-MacBook-Pro.local>
2022-04-19 15:46:02 +0900

    send req to web api and get res when clicked

--- src/components/jpyc/html-button.jsx
+++ src/components/jpyc/html-button.jsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Button from '../button/button.jsx';
+import axios from 'axios';
 
 class HTMLButton extends React.Component {
     constructor (props) {
@@ -16,6 +17,24 @@ class HTMLButton extends React.Component {
                 )}
                 onClick={() => {
                     console.log("HTML");
+                    const formData = new FormData();
+                    formData.append('x', 100); // FIXME
+                    formData.append('y', 150); // FIXME
+                    axios.post(
+                        'https://ov6jpqlth2.execute-api.us-east-2.amazonaws.com/default/',
+                        formData,
+                        {
+                            headers: {
+                                'Content-Type': 'multipart/form-data'
+                            }
+                        }
+                    ).then(res => {
+                        console.log('Succeeded');
+                        console.log(res);
+                    }).catch(err => {
+                        console.log()`Failed... ${err}`;
+                    });
+
                 }}
             >
                 <span>Create HTML</span>
