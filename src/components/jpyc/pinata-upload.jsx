919eb95d4209d20c5d0e616f9b6874884ce659fe
kiiiyono <kiiiyono@gmail.com>
2022-04-20 13:17:33 +0900

    pinataconfig added to webpack.config.js

--- src/components/jpyc/pinata-upload.jsx
+++ src/components/jpyc/pinata-upload.jsx
@@ -19,6 +19,7 @@ class PinataUploadButton extends React.Component {
 }
 
 const handleFileUpload = function (fileInput) {
+  
   console.log(fileInput[0]);
 
   //  const reader = new FileReader();
@@ -26,8 +27,10 @@ const handleFileUpload = function (fileInput) {
 
   var data = new FormData();
 
+  var today =new Date().toISOString();
+
   data.append('file', file);
-  data.append('pinataMetadata', '{\n    "name":"test wkbk" \n}');
+  data.append('pinataMetadata', '{\n    "name":"'+ today+ '"\n}');
   data.append('pinataOptions', '{\n    "cidVersion":0\n}');
 
   console.log(data.file);
@@ -36,8 +39,8 @@ const handleFileUpload = function (fileInput) {
     method: 'post',
     url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
     headers: {
-      'pinata_api_key': '',
-      'pinata_secret_api_key': '',
+      'pinata_api_key': process.env.PINATA_API_KEY,
+      'pinata_secret_api_key': process.env.PINATA_SECRET_API_KEY,
       'content-type': 'multipart/form-data; boundary=--------------------------733383420271935845045958'
     },
     data: data
@@ -62,10 +65,6 @@ const jsonFileUpload = function (hash) {
 
   var hashText = hash;
 
-  // var hashText = hash.slice( 1, -1 ) ;
-  // console.log(hashText);
-
-
   var imageText = `https://gateway.pinata.cloud/ipfs/${hashText}`;
   console.log(imageText);
 
@@ -80,19 +79,19 @@ const jsonFileUpload = function (hash) {
       "animation_url": "${animationUrlText}"
     }`;
 
-  const json = JSON.stringify(jsonText);
+  var today =new Date().toISOString();
 
   var data = new FormData();
   data.append('file', new Blob([jsonText], { type: 'application/json' }));
-  data.append('pinataMetadata', '{\n    "name":"test wkbk" \n}');
+  data.append('pinataMetadata', `{\n    "name":"${today}"\n}`);
   data.append('pinataOptions', '{\n    "cidVersion":0\n}');
 
   var config = {
     method: 'post',
     url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
     headers: {
-      'pinata_api_key': '',
-      'pinata_secret_api_key': '',
+      'pinata_api_key': process.env.PINATA_API_KEY,
+      'pinata_secret_api_key': process.env.PINATA_SECRET_API_KEY,
       'content-type': 'multipart/form-data; boundary=--------------------------733383420271935845045958'
     },
     data: data
